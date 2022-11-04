e38f8f87674b5ded156070ec9adc643b3f237e70
luongvuc0622i1 <vuhienluong.td151218@gmail.com>
2022-11-04 08:19:32 +0700

    model

--- src/main/java/com/codegym/model/Product.java
+++ src/main/java/com/codegym/model/Product.java
@@ -1,4 +1,67 @@
 package com.codegym.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private int price;
+    private String description;
+
+    public Product(int id, String name, int price, String description) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public Product() {
+    }
+
+    public Product(String name, int price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Product[id=%d, name='%s', price='%s', description='%s']", id, name, price, description);
+    }
 }
